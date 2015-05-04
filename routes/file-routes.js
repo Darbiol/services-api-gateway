0726aa7db6e04036b55b119de9109f70876a1c54
Raymond Torino <torino.raymond@gmail.com>
2015-05-04 22:13:08 +0800

    chore: pipe to AMQP stream

--- routes/file-routes.js
+++ routes/file-routes.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var fs = require('fs');
+var amqp = require('amqp');
+var amqpStream = require('amqp-stream');
 
 module.exports = [
   {
@@ -13,28 +15,36 @@ module.exports = [
       payload: {
         output: 'stream',
         parse: true,
-        allow: 'multipart/form-data'
+        allow: 'multipart/form-data',
+        maxBytes: 10485760 // 10MB
       },
       handler: function (request, reply) {
         var payload = request.payload;
 
         if (payload.file) {
-          var name = payload.file.hapi.filename;
-          var path = process.cwd() + '/uploads/' + name;
-          var file = fs.createWriteStream(path);
+          // create an AMPQ RPC client
+          var connection = amqp.createConnection({host: 'boot2docker'});
+          var amqpStreamOpts = {
+              connection: connection,
+              exchange: 'req-res.files-exchange',
+              routingKey: 'v1.files.upload'
+          };
 
-          file.on('error', function (err) {
-            console.error(err)
-          });
+          amqpStream(amqpStreamOpts, function (err, rpcStream) {
+            rpcStream.createCorrelatedRequest(function (err, fileUploader) {
+              payload.file.pipe(fileUploader);
 
-          payload.file.pipe(file);
+              fileUploader.on('end', function (err) {
+                connection.end.bind(connection);
 
-          payload.file.on('end', function (err) {
-            var ret = {
-              filename: payload.file.hapi.filename,
-              headers: payload.file.hapi.headers
-            };
-            reply(JSON.stringify(ret));
+                var ret = {
+                  filename: payload.file.hapi.filename,
+                  headers: payload.file.hapi.headers
+                };
+
+                reply(JSON.stringify(ret));
+              });
+            });
           });
         }
       }
